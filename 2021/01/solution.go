a32263b8c0727fe7f4955d04d59fb098d7b1980a
Alexey Shytikov <alexey.shytikov@klarna.com>
2021-12-01 14:42:55 +0100

    Day 01 / Puzzle 01

---
+++ 2021/01/solution.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	first()
+}
+
+func first() {
+	// Counter for increased depth measurements
+	counter := 0
+
+	// Previous depth value – intentionally set to big number
+	previous := math.MaxInt16
+	for _, measurement := range readInput("input.txt") {
+		current, err := strconv.Atoi(measurement)
+
+		if err != nil {
+			panic(fmt.Errorf("cannot convert measurement to number"))
+		}
+
+		if current-previous > 0 {
+			counter++
+		}
+
+		previous = current
+	}
+
+	fmt.Println(counter)
+}
+
+func readInput(filename string) []string {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Errorf("cannot find input file"))
+	}
+
+	return strings.Split(string(content), "\n")
+}
