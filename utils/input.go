6304006a081c7397604195e87c9788e6435f5afb
Alexey Shytikov <alexey.shytikov@klarna.com>
2021-12-02 13:37:10 +0100

    Introducing function to read directions

--- utils/input.go
+++ utils/input.go
@@ -24,5 +24,45 @@ func ReadNumbers(filename string) []int {
 		result = append(result, number)
 	}
 
+	if len(result) == 0 {
+		return nil
+	}
+
+	return result
+}
+
+type Direction struct {
+	Side     string
+	Distance int
+}
+
+// ReadDirections will parse file provided in format:
+//
+// stringKey1 intValue1\n
+// stringKey2 intValue2\n
+func ReadDirections(filename string) []Direction {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil
+	}
+
+	result := make([]Direction, 0)
+
+	for _, line := range strings.Split(string(content), "\n") {
+		chunks := strings.Split(line, " ")
+
+		distance, err := strconv.Atoi(chunks[1])
+
+		if err != nil {
+			continue
+		}
+
+		result = append(result, Direction{chunks[0], distance})
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
 	return result
 }
