73c187d3592342ae621f686d0d75edc5485f9090
Alexey Shytikov <alexey.shytikov@klarna.com>
2021-12-06 08:47:35 +0100

    Finaly finding time for day 03 second part

--- 2021/03/solution.go
+++ 2021/03/solution.go
@@ -23,7 +23,9 @@ func main() {
 }
 
 func solvePuzzleA(input [][]int, result chan int) {
-	half := len(input) / 2
+	// Idea is to sum all values per digit position, and when compare with half-length of the input.
+	// If sum exceeds it this means it was more `1` in this sum than `0`, and vice versa
+	threshold := len(input) / 2
 	length := len(input[0])
 
 	// Creating a slice to accumulate sums of digits in each position
@@ -39,7 +41,7 @@ func solvePuzzleA(input [][]int, result chan int) {
 	// Epsilon – least common digit
 	ε := make([]int, length)
 	for i, digit := range accumulator {
-		if digit > half {
+		if digit > threshold {
 			γ[i] = 1
 			ε[i] = 0
 		} else {
@@ -52,6 +54,68 @@ func solvePuzzleA(input [][]int, result chan int) {
 }
 
 func solvePuzzleB(input [][]int, result chan int) {
+	notFiltered := input
+	o2Level := 0
+	co2Level := 0
 
-	result <- 0
+	for i := 0; i < len(input[0]); i++ {
+		filteredBy0 := make([][]int, 0)
+		filteredBy1 := make([][]int, 0)
+
+		count0 := 0
+		count1 := 0
+
+		for _, number := range notFiltered {
+			if number[i] == 0 {
+				count0++
+				filteredBy0 = append(filteredBy0, number)
+			} else {
+				count1++
+				filteredBy1 = append(filteredBy1, number)
+			}
+		}
+
+		if count0 < count1 || count0 == count1 {
+			notFiltered = filteredBy1
+		} else {
+			notFiltered = filteredBy0
+		}
+
+		if len(notFiltered) == 1 {
+			o2Level = utils.LooseBinaryToInt(notFiltered[0])
+			break
+		}
+	}
+
+	notFiltered = input
+	for i := 0; i < len(input[0]); i++ {
+		filteredBy0 := make([][]int, 0)
+		filteredBy1 := make([][]int, 0)
+
+		count0 := 0
+		count1 := 0
+
+		for _, number := range notFiltered {
+			if number[i] == 0 {
+				count0++
+				filteredBy0 = append(filteredBy0, number)
+			} else {
+				count1++
+				filteredBy1 = append(filteredBy1, number)
+			}
+		}
+
+		if count0 < count1 || count0 == count1 {
+			notFiltered = filteredBy0
+		} else {
+			notFiltered = filteredBy1
+		}
+
+		if len(notFiltered) == 1 {
+			co2Level = utils.LooseBinaryToInt(notFiltered[0])
+			break
+		}
+	}
+
+	result <- o2Level*co2Level
 }
