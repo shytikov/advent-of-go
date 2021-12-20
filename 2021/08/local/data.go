9ac098cde7c632a7c9cac421b46045967cf84dd8
Alexey Shytikov <alexey.shytikov@gmail.com>
2021-12-20 23:50:25 +0100

    Implementing first part of day 08

--- 2021/08/local/data.go
+++ 2021/08/local/data.go
@@ -1,6 +1,89 @@
 package local
 
-type Data struct {
-	Patterns []string
-	Digits []Digit
+import (
+	"io/ioutil"
+	"strings"
+	"sync"
+)
+
+type Data []Entry
+
+type Entry struct {
+	Patterns []Digit
+	Readings []Digit
+}
+
+// Digit represent a number displayed on 7 segment indicator
+type Digit struct {
+	Original string
+	Decoded  int
+}
+
+func Read(filename string) Data {
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil
+	}
+
+	return parseData(string(content))
+}
+
+func parseData(content string) (result Data) {
+	lines := strings.Split(content, "\n")
+	count := len(lines)
+
+	result = make(Data, count)
+
+	var wg sync.WaitGroup
+
+	for i, line := range lines {
+		wg.Add(1)
+
+		go func(index int, definition string) {
+			defer wg.Done()
+			chunks := strings.Split(definition, " | ")
+
+			result[index] = Entry{
+				Patterns: parsePart(chunks[0]),
+				Readings: parsePart(chunks[1]),
+			}
+
+		}(i, line)
+	}
+
+	wg.Wait()
+
+	return
+}
+
+func parsePart(definition string) (result []Digit) {
+	chunks := strings.Split(definition, " ")
+
+	result = make([]Digit, len(chunks))
+
+	for i, digit := range chunks {
+		result[i] = parseDigit(digit)
+	}
+
+	return
+}
+
+func parseDigit(definition string) (result Digit) {
+	result.Original = definition
+
+	switch len(definition) {
+	case 2:
+		result.Decoded = 1
+	case 3:
+		result.Decoded = 7
+	case 4:
+		result.Decoded = 4
+	case 7:
+		result.Decoded = 8
+	default:
+		result.Decoded = -1
+	}
+
+	return
 }
--- 2021/08/solution.go
+++ 2021/08/solution.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/shytikov/advent-of-go/shared"
+
+	"github.com/shytikov/advent-of-go/2021/08/local"
 )
 
 func main() {
-	input := shared.ReadIntSlicesFromLines("./input.txt")
+	input := local.Read("./input.txt")
 
 	if input != nil && len(input) > 0 {
 		resultA := make(chan int)
@@ -22,10 +23,21 @@ func main() {
 	}
 }
 
-func solvePuzzleA(input [][]int, result chan int) {
-	result <- 0
+func solvePuzzleA(input local.Data, result chan int) {
+	count := 0
+
+	for i := 0; i < len(input); i++ {
+		for j := 0; j < len(input[i].Readings); j++ {
+			test := input[i].Readings[j].Decoded
+			if test == 1 || test == 4 || test == 7 || test == 8 {
+				count++
+			}
+		}
+	}
+
+	result <- count
 }
 
-func solvePuzzleB(input [][]int, result chan int) {
+func solvePuzzleB(input local.Data, result chan int) {
 	result <- 0
 }
