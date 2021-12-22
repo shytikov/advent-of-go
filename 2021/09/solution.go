d301b1baaca5b2cab14ac94eec304654c6e3ebcd
Alexey Shytikov <alexey.shytikov@klarna.com>
2021-12-22 11:34:42 +0100

    Solving task for first part of day 09

--- 2021/09/solution.go
+++ 2021/09/solution.go
@@ -23,7 +23,42 @@ func main() {
 }
 
 func solvePuzzleA(input [][]int, result chan int) {
-	result <- 0
+	risk := 0
+
+	// Vectors to adjacent locations
+	directions := [][]int{
+		{0, 1},
+		{0, -1},
+		{1, 0},
+		{-1, 0},
+	}
+
+	// Iterating through lines
+	for i := 0; i < len(input); i++ {
+		// Iterating through columns
+		for j := 0; j < len(input[i]); j++ {
+			// Flag indicating that we have found the lowest location
+			lowest := true
+			current := input[i][j]
+
+			for _, vector := range directions {
+				x := i + vector[0]
+				y := j + vector[1]
+
+				// We're not checking outside the boundaries. Boundaries are walls.
+				// We assume our lever is lower than any wall we encounter
+				if x >= 0 && y >= 0 && x < len(input) && y < len(input[i]) {
+					lowest = lowest && current < input[x][y]
+				}
+			}
+
+			if lowest {
+				risk += current + 1
+			}
+		}
+	}
+
+	result <- risk
 }
 
 func solvePuzzleB(input [][]int, result chan int) {
