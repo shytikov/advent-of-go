94cf5d59aa67d7d7e4a4c7dcdeea98956c607d7f
Alexey Shytikov <alexey.shytikov@gmail.com>
2021-12-27 13:02:42 +0100

    Adding puzzle input for day 11

---
+++ 2021/11/solution.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shytikov/advent-of-go/shared"
+)
+
+func main() {
+	input := shared.ReadRuneSlicesFromLines("./input.txt")
+
+	if input != nil && len(input) > 0 {
+		resultA := make(chan int)
+		resultB := make(chan int)
+
+		go solvePuzzleA(input, resultA)
+		go solvePuzzleB(input, resultB)
+
+		fmt.Println(<-resultA)
+		fmt.Println(<-resultB)
+	} else {
+		panic("failure when reading input data")
+	}
+}
+
+func solvePuzzleA(input [][]rune, result chan int) {
+	result <- 0
+}
+
+func solvePuzzleB(input [][]rune, result chan int) {
+	result <- 0
+}
